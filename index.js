33b63686b192db6364db0e46c68edd4b4da4717b
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-03-01 21:20:27 +0530

    Basic express http server created

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`running at http://localhost:${port}`);
+});
